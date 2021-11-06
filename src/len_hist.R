ac42f5aa95c956d31efd5d0ee04ad18b846440b6
Marat <61361613+Mrrrat@users.noreply.github.com>
2021-11-06 15:03:45 +0300

    Added files

---
+++ src/len_hist.R
@@ -0,0 +1,23 @@
+source('lib.R')
+
+###
+
+NAME <- 'DeepZ'
+#NAME <- 'H2AFZ_A549.ENCFF107KLN.hg19'
+#NAME <- 'H2AFZ_A549.ENCFF107KLN.hg38'
+#NAME <- 'H2AFZ_A549.ENCFF615OJI.hg19'
+#NAME <- 'H2AFZ_A549.ENCFF615OJI.hg38'
+
+###
+
+bed_df <- read.delim(paste0(DATA_DIR, NAME, '.bed'), as.is = TRUE, header = FALSE)
+#colnames(bed_df) <- c('chrom', 'start', 'end', 'name', 'score')
+colnames(bed_df) <- c('chrom', 'start', 'end')
+bed_df$len <- bed_df$end - bed_df$start
+
+ggplot(bed_df) +
+  aes(x = len) +
+  geom_histogram() +
+  ggtitle(NAME, subtitle = sprintf('Number of peaks = %s', nrow(bed_df))) +
+  theme_bw()
+ggsave(paste0('len_hist.', NAME, '.pdf'), path = OUT_DIR)
---
+++ src/lib.R
@@ -0,0 +1,9 @@
+library(ggplot2)
+library(dplyr)
+library(tidyr)   # replace_na
+library(tibble)  # column_to_rownames
+
+###
+
+DATA_DIR <- 'D:/Downloads/BioInfo/data/'
+OUT_DIR <- 'D:/Downloads/BioInfo/images/'
